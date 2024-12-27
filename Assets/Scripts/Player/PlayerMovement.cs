c76d2eeb6e19f2487ddf74b0f13c17204327c06d
EvanMcRae <evanmmcrae@gmail.com>
2024-12-27 16:54:44 -0500

    Dodgeroll mechanic

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,15 +4,25 @@ public class PlayerMovement : MonoBehaviour
 {
     float moveX, moveY;
     Rigidbody2D rb;
+    BoxCollider2D col;
+
     Vector3 targetVelocity, velocity = Vector3.zero;
     [Range(0, .3f)][SerializeField] private float movementSmoothing = 0.01f;
     [SerializeField] private float speed = 10.0f;
     private float calculatedSpeed = 10.0f;
+    public bool dodgeroll = false;
+    private bool canDodgeroll = true;
+    private float dodgerollTime, dodgerollResetTime;
+    [SerializeField] private float dodgerollLength = 0.3f;
+    [SerializeField] private float dodgerollCooldown = 0.3f;
+    [SerializeField] private float dodgerollSpeedMultiplier = 1.3f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<BoxCollider2D>();
+        dodgerollTime = float.PositiveInfinity;
     }
 
     // Update is called once per frame
@@ -20,11 +30,34 @@ public class PlayerMovement : MonoBehaviour
     {
         moveX = Input.GetAxisRaw("Horizontal");
         moveY = Input.GetAxisRaw("Vertical");
+        if (Input.GetKeyDown(KeyCode.LeftShift) && !dodgeroll && !(moveX == 0 && moveY == 0))
+        {
+            dodgeroll = true;
+            canDodgeroll = false;
+            dodgerollTime = Time.time + dodgerollLength;
+            dodgerollResetTime = Time.time + dodgerollLength + dodgerollCooldown;
+            col.enabled = false;
+        }
+
+        if (dodgeroll && Time.time >= dodgerollTime)
+        {
+            dodgeroll = false;
+            col.enabled = true;
+        }
+
+        if (!canDodgeroll && Time.time >= dodgerollResetTime)
+        {
+            canDodgeroll = true;
+        }
     }
 
     private void FixedUpdate()
     {
-        calculatedSpeed = speed; // TODO add any speed modifiers here (e.g. dodgeroll)
+        calculatedSpeed = speed;
+        if (dodgeroll)
+        {
+            calculatedSpeed *= dodgerollSpeedMultiplier;
+        }
 
         targetVelocity = new Vector2(moveX * calculatedSpeed, moveY * calculatedSpeed);
         if (targetVelocity.magnitude > calculatedSpeed)
