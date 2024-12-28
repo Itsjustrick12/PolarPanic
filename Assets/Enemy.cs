32dd7c51d870a09f78477b7f7f516ec9b2bb682b
Itsjustrick12 <Rickmjenkins900@gmail.com>
2024-12-27 21:44:26 -0500

    Basic Enemy Game Object
    Runs away if too close, gets into position, and holds if within distance

---
+++ Assets/Enemy.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+
+public enum EnemyState
+{
+    RunAway,
+    Hold,
+    MoveTowards
+}
+
+
+public class Enemy : MonoBehaviour
+{
+    
+    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float runAwayMult = 1.25f;
+    [SerializeField] Rigidbody2D rb;
+
+    [SerializeField] float minDistance = 3f;
+    [SerializeField] float shootRange = 5f;
+    [SerializeField] float fireRate = 1f;
+    private Vector2 playerDirection;
+    private EnemyState state;
+
+    private Transform player;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        player = GameManager.instance.player.transform;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 toPlayer = player.position - transform.position;
+        playerDirection = toPlayer.normalized;
+        
+        if (toPlayer.magnitude <= minDistance)
+        {
+            //Run Away
+            state = EnemyState.RunAway;
+            
+        }
+        else if (toPlayer.magnitude <= shootRange)
+        {
+            //stay put and shoot at player
+            state = EnemyState.Hold;
+        }
+        else
+        {
+            //move towards player
+            state = EnemyState.MoveTowards;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        DetermineMovement();
+    }
+
+    private void DetermineMovement()
+    {
+        switch (state)
+        {
+            case EnemyState.RunAway:
+                rb.linearVelocity = playerDirection * runAwayMult * -moveSpeed;
+                break;
+            case EnemyState.Hold:
+                rb.linearVelocity = Vector2.zero;
+                break;
+            case EnemyState.MoveTowards:
+                rb.linearVelocity = playerDirection * moveSpeed;
+                break;
+        }
+    }
+}
---
+++ Assets/GameManager.cs
@@ -0,0 +1,21 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public GameObject player;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
